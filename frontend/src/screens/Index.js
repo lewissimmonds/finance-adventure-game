b635926a124284301c1563460d6d2b43d9326eae
Lewis Simmonds <simmondslewis5@gmail.com>
2024-03-09 19:58:31 +0000

    changed index screen, looks better?

--- frontend/src/screens/Index.js
+++ frontend/src/screens/Index.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 // Import the font files
 import AdventureRequestFont from "../assets/fonts/AdventureRequest-j8W9.ttf";
 import AllerDisplayStdRgFont from "../assets/fonts/AllerDisplay_Std_Rg.ttf";
+import backgroundImage from "../assets/images/background.jpg";
 
 const IndexPage = () => {
   return (
@@ -41,33 +42,40 @@ const IndexPage = () => {
 // Updated styles object
 const styles = {
   container: {
-    position: "absolute", // Add this line
-    top: 0, // Add this line
-    left: 0, // Add this line
-    width: "100%", // Add this line
-    height: "100%", // Add this line
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
     textAlign: "center",
-    backgroundColor: "lightgreen",
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: "cover", // Add this line to make the image fit the container
   },
   headerBox: {
+    position: "relative", // Change this to 'relative'
     backgroundColor: "green",
     padding: "20px",
     borderRadius: "10px",
     display: "inline-block",
     marginTop: "100px",
     zIndex: 2, // Ensure the header is above the description box
+    marginLeft: "1000px", // Adjust as necessary for centering
+    boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)", // Add this line
   },
   descriptionBox: {
-    position: "absolute", // Position absolutely to layer under the header
-    top: "120px", // Adjust this value to overlap under the header box
+    position: "relative", // Change this to 'relative'
+    top: "0", // Set this to '0'
     left: "50%",
     transform: "translateX(-50%)", // Center the description box
     width: "30%", // Adjust width as needed
-    backgroundColor: "#f0f0f0", // Background color for the description box
-    padding: "20px",
+    backgroundColor: "#00B300", // Background color for the description box
+    padding: "10px",
+    paddingTop: "20px", // Adjust as necessary for spacing from the header box
     borderRadius: "10px",
     zIndex: 1, // Lower z-index than headerBox to appear underneath
-    marginTop: "100px", // Adjust as necessary for spacing from the header box
+    marginTop: "-15px", // Adjust as necessary for spacing from the header box
+    marginLeft: "500px", // Adjust as necessary for centering
+    boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)", // Add this line
   },
   header: {
     color: "yellow",
@@ -79,21 +87,25 @@ const styles = {
   description: {
     fontFamily: "AllerDisplayStdRg",
     fontSize: "1.2em",
-    color: "black",
+    color: "#FFDA29",
+    textShadow: "2px 2px 4px #000000",
   },
   button: {
-    fontFamily: "AdventureRequest-j8W9",
+    fontFamily: "AllerDisplayStdRg",
     display: "inline-block",
     padding: "20px 40px",
-    fontSize: "1.5em",
+    fontSize: "2em",
     color: "white",
     backgroundColor: "green",
     border: "none",
-    borderRadius: "5px",
+    borderRadius: "15px",
     cursor: "pointer",
     textDecoration: "none",
-    marginTop: "250px", // Adjust as necessary for spacing from the description
+    marginTop: "200px", // Adjust as necessary for spacing from the description
     width: "30%",
+    textShadow: "2px 2px 4px #000000",
+    marginLeft: "1000px", // Adjust as necessary for centering
+    boxShadow: "0px 4px 8px rgba(0, 0, 0, 0.2)", // Add this line
   },
 };
 
